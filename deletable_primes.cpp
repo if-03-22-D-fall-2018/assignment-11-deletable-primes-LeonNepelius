f74dd39b32a7169bf24336ffad2c1d82c9c06e99
LeonNepelius <leon@nepelius.at>
2019-01-14 09:14:37 -0800

    started

--- deletable_primes.cpp
+++ deletable_primes.cpp
@@ -10,3 +10,30 @@
  * Implementation of deletable_primes.h.
  * ----------------------------------------------------------
  */
+#include "deletable_primes.h"
+
+ unsigned long remove_digit(int index, unsigned long n)
+ {
+   if (is_prime(n))
+   {
+     
+   }
+   return 0;
+ }
+
+ int get_ways(unsigned long p)
+ {
+   return 0;
+ }
+
+ bool is_prime(unsigned long n)
+ {
+   if (n <= 1) return false;
+     if (n % 2 == 0 && n > 2) return 0;
+     for(unsigned int i = 3; i < n / 2; i+= 2)
+     {
+         if (n % i == 0)
+             return false;
+     }
+   return true;
+ }
--- deletable_primes.h
+++ deletable_primes.h
@@ -13,4 +13,10 @@
 #ifndef ___DELETABLE_PRIMES
 #define ___DELETABLE_PRIMES
 
+unsigned long remove_digit(int index, unsigned long n);
+
+int get_ways(unsigned long p);
+
+bool is_prime(unsigned long n);
+
 #endif
